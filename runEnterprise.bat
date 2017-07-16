94f050fb1af3b9e77f6251ea659c686dbc956521
zullusa <zyu@bk.ru>
2017-07-16 10:31:43 +0300

    runEnterprise.bat

--- runEnterprise.bat
+++ runEnterprise.bat
@@ -1,7 +1,10 @@
 @echo off
+mode con cp select=1251>nul
 call %~dp0setEnvironment.bat
 call notepad %basesfile%
 
 for /F "usebackq delims=;" %%I in ("%basesfile%") do (
+cls
+echo Запуск %%I
 "%onecexe%" ENTERPRISE /%basetype% "%server%\%%I" /AppAutoCheckMode
 )
